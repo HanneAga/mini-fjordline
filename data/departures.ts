176be59d1d1aa11ff45c8a2350e337a432543bbc
Hanne Aga <hannekarineaga@gmail.com>
2026-04-07 19:54:43 +0200

    feat: add departure type and mock data

---
+++ data/departures.ts
@@ -0,0 +1,44 @@
+import { Departure } from '../types/departure'
+
+export const departures: Departure[] = [
+  {
+    id: 'bergen-stavanger-20260501-0700',
+    from: 'Bergen',
+    to: 'Stavanger',
+    date: '2026-05-01',
+    departureTime: '07:00',
+    arrivalTime: '12:00',
+    durationMinutes: 300,
+    price: 499,
+  },
+  {
+    id: 'bergen-stavanger-20260501-1300',
+    from: 'Bergen',
+    to: 'Stavanger',
+    date: '2026-05-01',
+    departureTime: '13:00',
+    arrivalTime: '18:00',
+    durationMinutes: 300,
+    price: 549,
+  },
+  {
+    id: 'bergen-hirtshals-20260502-0900',
+    from: 'Bergen',
+    to: 'Hirtshals',
+    date: '2026-05-02',
+    departureTime: '09:00',
+    arrivalTime: '21:30',
+    durationMinutes: 750,
+    price: 1299,
+  },
+  {
+    id: 'bergen-hirtshals-20260502-1900',
+    from: 'Bergen',
+    to: 'Hirtshals',
+    date: '2026-05-02',
+    departureTime: '19:00',
+    arrivalTime: '07:30',
+    durationMinutes: 750,
+    price: 1349,
+  },
+]
---
+++ types/departure.ts
@@ -0,0 +1,10 @@
+export type Departure = {
+  id: string
+  from: string
+  to: string
+  date: string
+  departureTime: string
+  arrivalTime: string
+  durationMinutes: number
+  price: number
+}
